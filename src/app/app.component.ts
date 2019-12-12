6130d1480a2753ee8bdba24a3dcb9829c9e8bb3e
Scott <scott.penneman@gmail.com>
2019-12-11 19:23:36 -0600

    Finished Assignment #6, 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
 }
