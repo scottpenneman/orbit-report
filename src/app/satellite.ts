3f15343f16dd0c5eb871612007a6f37ecbf1746e
Scott <scott.penneman@gmail.com>
2019-12-17 20:56:02 -0600

    Completed Assignment #6 Sans Bonus Missions

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,9 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        return this.type.toLowerCase() === 'space debris';
+    }
+
 }
